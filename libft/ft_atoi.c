1abcbdf2be6462d87380606b20e34eb65fe45cf8
이준석 <seok@MacBookPro.local>
2020-12-23 16:54:56 +0900

    libft

---
+++ libft/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junseole <junseole@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 17:42:03 by junseole          #+#    #+#             */
+/*   Updated: 2020/12/23 16:32:13 by junseole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(char *str)
+{
+	int i;
+	int result;
+
+	result = 0;
+	i = 0;
+	while (*str == '\t' || *str == '\n'
+			|| *str == '\v' || *str == '\f'
+			|| *str == '\r' || *str == ' ')
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			i++;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result *= 10;
+		result += (*str - '0');
+		str++;
+	}
+	if (i % 2 == 1)
+		return ((-1) * result);
+	return (result);
+}
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junseole <junseole@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 13:40:40 by junseole          #+#    #+#             */
+/*   Updated: 2020/12/23 16:32:01 by junseole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
