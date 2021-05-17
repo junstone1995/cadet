7a7032f69272dd20258cb13cc04cd137d57cb1af
Junseok Lee <boris0730@gmail.com>
2021-05-17 22:42:45 +0900

    get_next_line 구현

---
+++ exam-rank02/get_next_line/get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	get_next_line(char **line);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *s);
+char	*ft_strchr(char *s, char c);
+char	*ft_strndup(char *s1, int n);
+
+#endif
