a43d00ddc8180596b6a3e122d83633a9940dbc98
Junseok Lee <boris0730@gmail.com>
2021-07-16 22:18:46 +0900

    mini

---
+++ minitalk/bonus/server.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junseole <junseole@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 20:55:51 by junseole          #+#    #+#             */
+/*   Updated: 2021/07/15 21:04:13 by junseole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.h"
+#include <stdio.h>
+
+void	fnsig(int sig)
+{
+	static char		word = 0;
+	static int		cnt = 0;
+
+	word <<= 1;
+	if (sig == 30)
+		word += 1;
+	if (cnt == 0)
+	{
+		write(1, &word, 1);
+		word = 0;
+		cnt = 0;
+	}
+	usleep(1);
+}
+
+int		main(void)
+{
+	signal(SIGUSR1, (void *)fnsig);
+	signal(SIGUSR2, (void *)fnsig);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
