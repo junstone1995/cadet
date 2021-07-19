094954f32e003aca57c72b8dd583c47df1094c12
Junseok Lee <boris0730@gmail.com>
2021-07-19 19:14:23 +0900

    fix client

--- minitalk/client_bonus.h
+++ minitalk/client_bonus.h
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server_bonus.h                                     :+:      :+:    :+:   */
+/*   client_bonus.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junseole <junseole@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/15 20:56:40 by junseole          #+#    #+#             */
-/*   Updated: 2021/07/19 18:31:27 by junseole         ###   ########.fr       */
+/*   Created: 2021/07/15 21:05:14 by junseole          #+#    #+#             */
+/*   Updated: 2021/07/19 19:08:44 by junseole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_BONUS_H
-# define SERVER_BONUS_H
+#ifndef CLIENT_BONUS_H
+# define CLIENT_BONUS_H
 
 # include "subtool.h"
+# include <stdlib.h>
 
-void	fnsig(int sig);
+void	bit_send(int pid, char word, char num);
+void	send_unit(int pid, char word);
+int		ft_nbrlen(long n);
+char	*ft_itoa(int n, char *base);
 #endif
