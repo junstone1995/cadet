87fcd19c296c138020ebe6700ab87ab4951882e7
Junseok Lee <boris0730@gmail.com>
2021-09-22 21:21:31 +0900

    fix

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,44 +6,11 @@
 /*   By: junseole <junseole@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 17:27:31 by junseole          #+#    #+#             */
-/*   Updated: 2021/09/21 21:47:25 by junseole         ###   ########.fr       */
+/*   Updated: 2021/09/21 22:35:13 by junseole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
-
-int is_full(t_deque *q)
-{
-	if (((q->rear + 1) % deque_max) == q->front)
-		return (1);
-	return (0);
-}
-
-int is_empty(t_deque *q)
-{
-	if (q->front == q->rear)
-		return (1);
-	return (0);
-}
-
-void deque_print(t_deque *q) {
-
-	int i = (q->front + 1) % deque_max;
-
-	printf("DEQUE(front=%d rear=%d) = ",q->front,q->rear);
-
-	if (is_empty(q)) {
-		printf("공백큐\n");
-		return ;
-	}
-	while (i!=q->rear) {
-		printf("%d | ",q->value[i]);
-	i = (i + 1) % deque_max;
-	}
-	printf("%d\n",q->value[i]);
-
-}
 
 int		main(int argc, char **argv)
 {
@@ -52,11 +19,19 @@ int		main(int argc, char **argv)
 
 	if (argc <= 1)
 		return (0);
+
 	init_deque(&a);
 	init_deque(&b);
 
+	
+
+	push_swap(a, b , )
 	free(a.value);
 	free(b.value);
-	return (0);
 }
 
+for (argc 개수): split해준 배열
+	if (split해준 배열 내부 인자 개수 > 2)
+		for (split 해준 배열 내부 인자 개수) split 해준 배열 내부 인자를 long long 형태로 바꿔준다
+		(long long인 이유 -> int를 넘어서는 숫자도 파악하기 위해) int_arr에 저장
+	else split 해준 배열 내부 인자를 long long 형태로 바꿔준다 (long long인 이유 -> int를 넘어서는 숫자도 파악하기 위해) int_arr에 저장
